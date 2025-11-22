c26b77e93d0ec6a4565b191e755b8e7c97775d95
Shashith-Malshan <malshanshashith318@gmail.com>
2025-11-22 23:43:43 +0530

    Use constructor dependency injection

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -2,6 +2,7 @@ package edu.icet.controller;
 
 import edu.icet.model.dto.Customer;
 import edu.icet.service.CustomerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class CustomerController {
-    @Autowired
-    CustomerService customerService;
+
+    final CustomerService customerService;
 
 
 
-    @GetMapping
+    @GetMapping("/customerInfo")
     public List<Customer> getAll(){
         return customerService.getAll();
 
